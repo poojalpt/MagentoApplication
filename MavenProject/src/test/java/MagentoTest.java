fd5ed54fe599e2ca3c7775814ea07ca712bbf003
pooja <pooja@DESKTOP-UIGGJ75>
2019-01-22 15:16:52 +0530

    second push

--- MavenProject/src/test/java/MagentoTest.java
+++ MavenProject/src/test/java/MagentoTest.java
@@ -2,7 +2,7 @@
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 
@@ -14,7 +14,7 @@ public class MagentoTest
 	public void magento()
 	{
 		
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		String url = "https://www.magento.com";
